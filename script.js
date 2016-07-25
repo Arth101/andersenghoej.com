3c20493a6f41bfdb53019ce2a4012a23a186f055
anders <anders@yacc.io>
2016-07-25 17:05:27 +0200

    Overlay system is now working. Effects has been added

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
-console.log("world");
+console.log("eyy world");
 
-document.querySelectorAll(".education").addEventListener("click", function() {
+document.querySelector("#plz").addEventListener("click", function() {
   console.log("clicked next");
-  document.querySelector(".overlay").style.display = "block";
+  document.querySelector(".overlay").className += " overlayShow";
 });
 
-document.querySelector(".close-overlay").addEventListener("click", function() {
-  document.querySelector(".overlay").style.display = "none";
-});
+function closeOverlay() {
+  console.log("work plz");
+  var crosses = document.querySelectorAll(".overlay");
+  for(var i = 0; i < crosses.length; i++){
+   //do something to each div like
+   crosses[i].className = "overlay";
+  }
+}
+
+function openOverlay(id) {
+  console.log("clicked!!!");
+  document.querySelector("#" + id + "-overlay").className += " overlayShow";
+};
