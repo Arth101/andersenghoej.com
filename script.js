f2aa05055035db346f77d2a7cabc5dd83563ccef
anders <anders@yacc.io>
2016-08-16 17:11:28 +0200

    changed everything up and began anew. Now stuff is better!

--- script.js
+++ script.js
@@ -20,3 +20,70 @@ window.onkeydown = function(evt) {
       closeOverlay();
    }
 }
+
+var boxes = [
+  {
+    "name" : "me",
+    "size" : "small",
+    "text" : "Anders Enghøj",
+    "image" : "linkedin.jpg",
+    "link" : "www.google.dk"
+  },
+  {
+    "name" : "Yacc.io",
+    "size" : "small",
+    "text" : "Anders Enghøj",
+    "image" : "yacc.png",
+    "link" : "www.google.dk"
+  },
+  {
+    "name" : "education",
+    "size" : "large",
+    "text" : "Anders eeee",
+    "image" : "facebook.jpg",
+    "link" : "www.google.dk"
+  },
+  {
+    "name" : "The 2nd Edit",
+    "size" : "small",
+    "text" : "Anders Enghøj",
+    "image" : "2ndEdit.png",
+    "link" : "www.google.dk"
+  },
+  {
+    "name" : "Mærkelex.dk",
+    "size" : "small",
+    "text" : "Anders Enghøj",
+    "image" : "maerkelex.svg",
+    "link" : "www.google.dk"
+  },
+  {
+    "name" : "NJORD",
+    "size" : "small",
+    "text" : "Anders Enghøj",
+    "image" : "njord.jpg",
+    "link" : "www.google.dk"
+  },
+  {
+    "name" : "Greve Parkour",
+    "size" : "small",
+    "text" : "Anders Enghøj",
+    "image" : "parkour.jpg",
+    "link" : "www.google.dk"
+  },
+  {
+    "name" : "harvikampfly.dk",
+    "size" : "small",
+    "text" : "Anders Enghøj",
+    "image" : "kampfly.jpg",
+    "link" : "www.google.dk"
+  }
+]
+
+boxes.forEach(function(box) {
+  console.log("name: ", box.name);
+  var div = document.createElement('div');
+  div.className = 'flex-item ' + box.size;
+  div.innerHTML = '<div class="flex-img" style="background-image: url(images/' + box.image + ')">' + box.name + '</div>';
+  document.querySelector('.flex-container').appendChild(div);
+})
