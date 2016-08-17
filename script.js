84e41eb8713be3c01243f1d093f75f774ff785b7
anders <anders@yacc.io>
2016-08-17 16:01:34 +0200

    lots of new changes. Stuff actually looks great now. Info is shown on hover

--- script.js
+++ script.js
@@ -23,59 +23,84 @@ window.onkeydown = function(evt) {
 
 var boxes = [
   {
-    "name" : "me",
+    "name" : "",
+    "id" : "me",
+    "type" : "personal",
     "size" : "small",
-    "text" : "Anders Enghøj",
+    "text" : "",
     "image" : "linkedin.jpg",
     "link" : "www.google.dk"
   },
   {
     "name" : "Yacc.io",
+    "id" : "yacc",
+    "type" : "personal",
     "size" : "small",
-    "text" : "Anders Enghøj",
+    "text" : "Current Employer",
     "image" : "yacc.png",
     "link" : "www.google.dk"
   },
   {
-    "name" : "education",
+    "name" : "Web Developer,<br />Multimedia Designer",
+    "id" : "education",
+    "type" : "personal",
     "size" : "large",
-    "text" : "Anders eeee",
-    "image" : "facebook.jpg",
+    "text" : "Skills & Education",
+    "color" : "rgba(18,82,151,1)",
     "link" : "www.google.dk"
   },
   {
     "name" : "The 2nd Edit",
+    "id" : "the2ndedit",
+    "type" : "job",
     "size" : "small",
-    "text" : "Anders Enghøj",
+    "text" : "Webshop",
     "image" : "2ndEdit.png",
     "link" : "www.google.dk"
   },
   {
-    "name" : "Mærkelex.dk",
+    "name" : "Mærke&#173;lex",
+    "id" : "maerkelex",
+    "type" : "job",
     "size" : "small",
-    "text" : "Anders Enghøj",
+    "text" : "Scouting badge catalog",
     "image" : "maerkelex.svg",
     "link" : "www.google.dk"
   },
   {
     "name" : "NJORD",
+    "id" : "njord",
+    "type" : "job",
     "size" : "small",
-    "text" : "Anders Enghøj",
+    "text" : "Wordpress webshop",
     "image" : "njord.jpg",
     "link" : "www.google.dk"
   },
   {
     "name" : "Greve Parkour",
+    "id" : "greveparkour",
+    "type" : "interest",
     "size" : "small",
     "text" : "Anders Enghøj",
     "image" : "parkour.jpg",
     "link" : "www.google.dk"
   },
   {
-    "name" : "harvikampfly.dk",
+    "name" : "harvi&#173;kampfly.dk",
+    "id" : "kampfly",
+    "type" : "interest",
     "size" : "small",
     "text" : "Anders Enghøj",
     "image" : "kampfly.jpg",
+    "link" : "http://harvikampfly.dk/"
+  },
+  {
+    "name" : "Strong Geeks",
+    "id" : "stronggeeks",
+    "type" : "interest",
+    "size" : "small",
+    "text" : "We had a lot of fun",
+    "image" : "stronggeeks.jpg",
     "link" : "www.google.dk"
   }
 ]
@@ -84,6 +109,23 @@ boxes.forEach(function(box) {
   console.log("name: ", box.name);
   var div = document.createElement('div');
   div.className = 'flex-item ' + box.size;
-  div.innerHTML = '<div class="flex-img" style="background-image: url(images/' + box.image + ')">' + box.name + '</div>';
-  document.querySelector('.flex-container').appendChild(div);
-})
+
+  var boxImage = box.image;
+  if(box.image == null){
+    boxImage = "background-color:" + box.color;
+  } else {
+    boxImage = "background-image: url(images/" + box.image + ")";
+  };
+
+  div.innerHTML = '<div class="flex-img" id="' + box.id + '" style="' + boxImage + '"><p>' + box.name + '<span>' + box.text + '</span></p><div class="valign"><p><strong>' + box.name +'</strong>' + box.text +'<a href="' + box.link + '"><i class="material-icons links">link</i></a></p></div></div>';
+
+  if(box.type == "personal"){
+    document.querySelector('#personal').appendChild(div);
+  }
+  if(box.type == "job"){
+    document.querySelector('#job').appendChild(div);
+  }
+  if(box.type == "interest"){
+    document.querySelector('#interest').appendChild(div);
+  }
+});
